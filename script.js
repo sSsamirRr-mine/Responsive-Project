e2932440faee1535cf5f8586dd50461a7e09f766
sSsamirRr-mine <sssamirrr4@gmail.com>
2024-11-15 14:54:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('menu').addEventListener('click', ()=>{
+    document.querySelector('ul').style.display = 'flex'
+})
+
+document.getElementById('x').addEventListener('click', ()=>{
+    document.querySelector('ul').style.display = 'none'
+})
